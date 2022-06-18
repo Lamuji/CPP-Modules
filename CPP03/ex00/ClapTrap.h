4ed920e76f48cc2814564135d18adbcd9bb7a0f9
ramzi <ramzifk@hotmail.fr>
2022-06-18 11:57:21 +0200

    ok

--- CPP03/ex00/ClapTrap.h
+++ CPP03/ex00/ClapTrap.h
@@ -6,7 +6,7 @@
 /*   By: ramzi <ramzi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 17:37:02 by ramzi             #+#    #+#             */
-/*   Updated: 2022/06/16 19:25:24 by ramzi            ###   ########.fr       */
+/*   Updated: 2022/06/17 18:36:29 by ramzi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CLAPTRAP_H
 
 #include <iostream>
+#include <string>
 
 class ClapTrap
 {
@@ -22,29 +23,31 @@ private :
 	std::string _Name;
 	int			_HitPoints;
 	int			_EnergyPoints;
-	int			_AttackDamage
+	int			_AttackDamage;
 
 public :
 
 	ClapTrap();
 	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const &copy);
 	~ClapTrap();
 
 ClapTrap & operator=(ClapTrap const &rhs);
 
 std::string	getName() const;
 int			getHitPoints() const;
-int			getEnergiePoints() const;
+int			getEnergyPoints() const;
 int			getAttackDamage() const;
 
 void		setName(std::string name);
 void		setHitPoints(int hitpoints);
-void		setEnergypoints(int energypoints);
+void		setEnergyPoints(int energypoints);
 void		setAttackDamage(int attackdamage);
 
 void		attack(const std::string & target);
 void		takeDamage(unsigned int amount);
 void		beRepaired(unsigned int amount);
-}
+
+};
 
 #endif 
